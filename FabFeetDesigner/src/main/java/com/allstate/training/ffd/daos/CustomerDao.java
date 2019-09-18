1fdc1589168a00575697dd2c004cad3f969af5ba
Shubham Prakash <flytoshubham@gmail.com>
2019-09-18 10:16:45 +0530

    dao methods

--- FabFeetDesigner/src/main/java/com/allstate/training/ffd/daos/CustomerDao.java
+++ FabFeetDesigner/src/main/java/com/allstate/training/ffd/daos/CustomerDao.java
@@ -1,5 +1,12 @@
 package com.allstate.training.ffd.daos;
 
 public interface CustomerDao {
+public void addCustomer(Customer c)throws BuisnessException;
+	public void updateCustomerPassword(int cid,  String newpass)throws BuisnessException;
+	public void updateCustomerAddress(int cid,String newaddress)throws BuisnessException;
+	public void updateCustomerPhone(int cid,String newphone)throws BuisnessException;
+	public void deleteCustomer(int cid)throws BuisnessException;
+	public List<Customer> getAllCustomer()throws BuisnessException;
+	public Customer customerLogin(String username, String password)throws BuisnessException;
 
 }
